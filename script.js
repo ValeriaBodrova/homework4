68a84e0f8bd4349f97afa121d326daccd35a8445
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-05 10:07:34 +0200

    Done task 1

---
+++ script.js
@@ -0,0 +1,10 @@
+//task1
+
+let number = parseInt(prompt("Enter a number"));
+let number2 = parseInt(prompt("Enter another number"));
+
+if (number > number2) {
+    alert(`${number} > ${number2}`);
+} else {
+    alert(`${number} < ${number2}`);
+}

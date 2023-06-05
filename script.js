1137ec054ac852db660d9fd12d09604a83accce9
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-05 14:32:36 +0200

    task 4 done

--- script.js
+++ script.js
@@ -41,3 +41,17 @@ if (count1 === integer1 && count2 !== integer2) {
 } else if(count1 !== integer1 && count2 !== integer2){
     alert("Number b is not a divisor of number a and Number a is not a divisor of number b ");
 }
+
+//task4
+
+let numberTask4 = parseInt(prompt("Enter a number"));
+
+let typeOfDigit = numberTask4 / 2;
+let int = Math.trunc(typeOfDigit);
+let lastDigit = numberTask4 % 10;
+
+if (typeOfDigit === int) {
+    alert(`The number is even. Last digit: ${lastDigit}`);
+} else {
+    alert(`The number is odd. Last digit: ${lastDigit}`);
+}

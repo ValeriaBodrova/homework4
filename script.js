5368e1fcfc2888f5dbc43c2e5ba5474f0d0f6176
ValeriaBodrova <110822668+ValeriaBodrova@users.noreply.github.com>
2023-06-05 13:22:29 +0200

    done task2

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ if (number > number2) {
 } else {
     alert(`${number} < ${number2}`);
 }
+
+
+//task2
+
+let distance1 = parseInt(prompt("Enter the distance in kilometers"));
+let distance2 = parseInt(prompt("Enter the distance in foots"));
+
+let count = distance2 * 0.0003048;
+
+if (distance1 > count) {
+    alert(`Second distance(${distance2}ft) < first distance(${distance1}km)  `);
+} else {
+    alert(`First distance(${distance1}km) < second distance(${distance2}ft)`);
+}
